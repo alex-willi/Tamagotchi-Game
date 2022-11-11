baabe3eeb1c2771fbec4db600b970cc1f47cb5f4
alex-willi <alex.lawson.williams@gmail.com>
2022-11-11 11:29:13 -0600

    added start button functions to display in game controles and remove start menu.

--- main.js
+++ main.js
@@ -8,6 +8,10 @@ const playValue = document.querySelector(".boredom-value")
 const feedBtn = document.querySelector("#feed")
 const hungerValue = document.querySelector(".hunger-value")
 
+const startBtn = document.querySelector("#start")
+const inGame = document.querySelector(".in-game")
+const startMenu = document.querySelector(".start")
+
 
 sleepBtn.addEventListener("click", () => {
 sleepValue.textContent++
@@ -17,4 +21,10 @@ playBtn.addEventListener("click", () => {
 })
 feedBtn.addEventListener("click", () => {
     hungerValue.textContent++
+})
+
+startBtn.addEventListener("click", () => {
+    inGame.style.display = "block"
+    startMenu.style.display = "none"
+    
 })
