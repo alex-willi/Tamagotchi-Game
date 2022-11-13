9b6792d3e712ee67853724cf870ae849aa5a8fbe
alex-willi <alex.lawson.williams@gmail.com>
2022-11-13 09:21:34 -0600

    Made Tamagotchi object and put values in

--- main.js
+++ main.js
@@ -1,51 +1,56 @@
+let Tamagotchi = {
+    petName: document.querySelector("#name"),
+    sleepValue: document.querySelector(".sleep-value"),
+    playValue: document.querySelector(".boredom-value"),
+    hungerValue: document.querySelector(".hunger-value"),
+    age: document.querySelector("#age-value"),
+    
+}
+console.log(Tamagotchi)
 const sleepBtn = document.querySelector("#sleep")
-let sleepValue = document.querySelector(".sleep-value")
 
 const playBtn = document.querySelector("#play")
-const playValue = document.querySelector(".boredom-value")
 
 
 const feedBtn = document.querySelector("#feed")
-const hungerValue = document.querySelector(".hunger-value")
 
 const startBtn = document.querySelector("#start")
 const inGame = document.querySelector(".in-game")
 const startMenu = document.querySelector(".start")
-let age = document.querySelector("#age-value")
 let generalInterval = 0
 
 
 sleepBtn.addEventListener("click", () => {
-    if(sleepValue.textContent < 10){
-        sleepValue.textContent++
+    if(Tamagotchi.sleepValue.textContent < 10){
+        Tamagotchi.sleepValue.textContent++
     }
 })
 playBtn.addEventListener("click", () => {
-    if (playValue.textContent < 10){
-    playValue.textContent++
+    if (Tamagotchi.playValue.textContent < 10){
+    Tamagotchi.playValue.textContent++
     }
 })
 feedBtn.addEventListener("click", () => {
-    if(hungerValue.textContent < 10){
-        hungerValue.textContent++
+    if(Tamagotchi.hungerValue.textContent < 10){
+        Tamagotchi.hungerValue.textContent++
     }
 })
 
 startBtn.addEventListener("click", () => {
     inGame.style.display = "block"
     startMenu.style.display = "none"
-    let petName =prompt("Enter a name for your Tamagotchi")
-    document.querySelector("#name").innerHTML = petName;
-    if (petName !== ""){
+    let promptName =prompt("Enter a name for your Tamagotchi")
+    Tamagotchi.petName.innerHTML = promptName;
+    if (Tamagotchi.petName !== ""){
         let gameTimer = setInterval(()=>{
             generalInterval = generalInterval +1
             if ((generalInterval % 10) === 0 ){
-                age.innerHTML++
+                Tamagotchi.age.innerHTML++
             }
             console.log(generalInterval)
-            let decreaseSleep = sleepValue.innerHTML = sleepValue.innerHTML -1
-            let decreasePlay = playValue.innerHTML = playValue.innerHTML -1
-            let decreaseHunger = hungerValue.innerHTML = hungerValue.innerHTML -1
+            let decreaseSleep = Tamagotchi.sleepValue.innerHTML = Tamagotchi.sleepValue.innerHTML -1
+            let decreasePlay = Tamagotchi.playValue.innerHTML = Tamagotchi.playValue.innerHTML -1
+            let decreaseHunger = Tamagotchi.hungerValue.innerHTML = Tamagotchi.hungerValue.innerHTML -1
             if (decreaseSleep === 0 || decreasePlay === 0 || decreaseHunger === 0){
                 clearInterval(gameTimer)
             setTimeout(()=>{
