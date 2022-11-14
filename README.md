1d55104fbb35f2457ab0595c23dc7546118af00c
alex-willi <alex.lawson.williams@gmail.com>
2022-11-13 20:08:16 -0600

    prettier used

--- README.md
+++ README.md
@@ -1,31 +1,30 @@
 # Tamagotchi-Game
+
 PProject 0
 User Story:
- 
+
 To start the user should see a start button. When clicked it should prompt the user to name their Tomigotchi
- 
+
 After naming the pet, the user should see a Tomagotchi pet in the middle of the screen that displays how it is feeling.
- 
+
 There should be 3 values of sleep, Boredom and hungry above the pet for the user to monitor.
- 
+
 Three buttons below the pet that correspond with the above values.
- 
+
 When buttons are clicked the pet values should lower and make the pet happier. This simulates interaction with the pet.
- 
+
 When the pet has no interaction the values go up signifying the level of unhappiness the pet is feeling.
- 
+
 If one of the three levels reaches a value of 10. the pet dies.
 
 When the pet dies, the game displays that you are a bad pet owner.
 
-
-
 pseudo coding exercise:
- 
+
 Start button should prompt to name pet
- 
+
 Update pet name from empty string
- 
+
 if pet name is entered display:
 feed value 0
 play value 0
@@ -34,33 +33,22 @@ buttons of:
 feed
 play
 sleep
- 
+
 set timer to count up by .5 sec
- 
+
 every time timer changes inresses values by 1
 if value is under five keep value green
 if one of the values reach 5 change background of that value to yellow
 if one of the values reach 8 change background of that value to red
-if one  value reaches 10:
+if one value reaches 10:
 alert user tamagotchi is dead
 stop timer
- 
+
 if buttons are pressed decrease values by 1
 Stop value from decrease below 0
- 
+
 Make tamagotchi
- 
+
 Define tamagotchi comprised of face and arms
- 
+
 When a value is in a green, yellow or red zone, change the position of face and arms.
-
-
-
-
-
-
-
-
-
-
-
--- main.js
+++ main.js
@@ -1,92 +1,88 @@
 let Tamagotchi = {
-    petName: document.querySelector("#name"),
-    sleepValue: document.querySelector(".sleep-value"),
-    playValue: document.querySelector(".boredom-value"),
-    hungerValue: document.querySelector(".hunger-value"),
-    age: document.querySelector("#age-value"),
-    
-}
-const sleepBtn = document.querySelector("#sleep")
+  petName: document.querySelector("#name"),
+  sleepValue: document.querySelector(".sleep-value"),
+  playValue: document.querySelector(".boredom-value"),
+  hungerValue: document.querySelector(".hunger-value"),
+  age: document.querySelector("#age-value"),
+};
+const sleepBtn = document.querySelector("#sleep");
 
-const playBtn = document.querySelector("#play")
+const playBtn = document.querySelector("#play");
 
+const feedBtn = document.querySelector("#feed");
 
-const feedBtn = document.querySelector("#feed")
+const startBtn = document.querySelector("#start");
 
-const startBtn = document.querySelector("#start")
-const hide = document.querySelector("#hide")
-const startMenu = document.querySelector(".start")
-let generalInterval = 0
-let nameInput = document.querySelector("#name-input")
-let gameOver = document.querySelector("#game-over")
-let playAgain = document.querySelector("#play-again")
-let icon = document.querySelector("#icon")
+const hide = document.querySelector("#hide");
 
+const startMenu = document.querySelector(".start");
+
+let generalInterval = 0;
+
+let nameInput = document.querySelector("#name-input");
+
+let gameOver = document.querySelector("#game-over");
+
+let playAgain = document.querySelector("#play-again");
+
+let icon = document.querySelector("#icon");
 
 sleepBtn.addEventListener("click", () => {
-    if(Tamagotchi.sleepValue.textContent > 0){
-        Tamagotchi.sleepValue.textContent--
-    }
-})
+  if (Tamagotchi.sleepValue.textContent > 0) {
+    Tamagotchi.sleepValue.textContent--;
+  }
+});
 playBtn.addEventListener("click", () => {
-    if (Tamagotchi.playValue.textContent > 0){
-    Tamagotchi.playValue.textContent--
-    }
-})
+  if (Tamagotchi.playValue.textContent > 0) {
+    Tamagotchi.playValue.textContent--;
+  }
+});
 feedBtn.addEventListener("click", () => {
-    if(Tamagotchi.hungerValue.textContent > 0){
-        Tamagotchi.hungerValue.textContent--
-    }
-})
+  if (Tamagotchi.hungerValue.textContent > 0) {
+    Tamagotchi.hungerValue.textContent--;
+  }
+});
 startBtn.addEventListener("click", () => {
-    hide.style.display = "block"
-    startMenu.style.display = "none"
-    Tamagotchi.petName.innerHTML = nameInput.value;
-    if (Tamagotchi.petName !== ""){
-        let gameTimer = setInterval(()=>{
-            generalInterval = generalInterval +1
-            if ((generalInterval % 10) === 0 ){
-                Tamagotchi.age.innerHTML++
-            }
-            let decreaseSleep = Tamagotchi.sleepValue.innerHTML++
-            let decreasePlay = Tamagotchi.playValue.innerHTML++
-            let decreaseHunger = Tamagotchi.hungerValue.innerHTML++
-            if (decreaseSleep < 4 || decreasePlay < 4 || decreaseHunger < 4){
-                icon.innerHTML = "😀"
-            }
-            if (decreaseSleep > 4 || decreasePlay > 4 || decreaseHunger > 4){
-                icon.innerHTML = "😐"
-            }
-            if (decreaseSleep > 8 || decreasePlay > 8 || decreaseHunger > 8){
-                icon.innerHTML = "😧"
-            }
-            if (decreaseSleep == 10 || decreasePlay == 10 || decreaseHunger == 10){
-                icon.innerHTML = "☠️"
-            }
-            if (decreaseSleep > 9 || decreasePlay > 9  || decreaseHunger > 9){
-                clearInterval(gameTimer)
-                gameOver.style.display = "inline"
-                playAgain.style.display = "block"
+  hide.style.display = "block";
+  startMenu.style.display = "none";
+  Tamagotchi.petName.innerHTML = nameInput.value;
+  if (Tamagotchi.petName !== "") {
+    let gameTimer = setInterval(() => {
+      generalInterval = generalInterval + 1;
+      if (generalInterval % 10 === 0) {
+        Tamagotchi.age.innerHTML++;
+      }
+      let decreaseSleep = Tamagotchi.sleepValue.innerHTML++;
+      let decreasePlay = Tamagotchi.playValue.innerHTML++;
+      let decreaseHunger = Tamagotchi.hungerValue.innerHTML++;
+      if (decreaseSleep < 4 || decreasePlay < 4 || decreaseHunger < 4) {
+        icon.innerHTML = "😀";
+      }
+      if (decreaseSleep > 4 || decreasePlay > 4 || decreaseHunger > 4) {
+        icon.innerHTML = "😐";
+      }
+      if (decreaseSleep > 8 || decreasePlay > 8 || decreaseHunger > 8) {
+        icon.innerHTML = "😧";
+      }
+      if (decreaseSleep == 10 || decreasePlay == 10 || decreaseHunger == 10) {
+        icon.innerHTML = "☠️";
+      }
+      if (decreaseSleep > 9 || decreasePlay > 9 || decreaseHunger > 9) {
+        clearInterval(gameTimer);
+        gameOver.style.display = "inline";
+        playAgain.style.display = "block";
+      }
+    }, 2000);
+  }
+});
 
-            }
-        },2000)
-    }
-})
-
-playAgain.addEventListener("click",() =>{
-    startMenu.style.display = "grid"
-    hide.style.display = "none"
-    playAgain.style.display = "none"
-    gameOver.style.display = "none"
-    Tamagotchi.sleepValue.innerHTML = 0
-    Tamagotchi.playValue.innerHTML = 0
-    Tamagotchi.hungerValue.innerHTML = 0
-    Tamagotchi.age.innerHTML = 0
-
-} )
-
-
-
-
-
-
+playAgain.addEventListener("click", () => {
+  startMenu.style.display = "grid";
+  hide.style.display = "none";
+  playAgain.style.display = "none";
+  gameOver.style.display = "none";
+  Tamagotchi.sleepValue.innerHTML = 0;
+  Tamagotchi.playValue.innerHTML = 0;
+  Tamagotchi.hungerValue.innerHTML = 0;
+  Tamagotchi.age.innerHTML = 0;
+});
